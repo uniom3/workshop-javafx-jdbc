0636da0f78d74cd1604afd690ab09912d1eeb1c2
uniom3 <deivid.mendonca@hotmail.com>
2019-11-19 22:20:18 -0300

    Seller TableView

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -41,6 +42,15 @@ public class SellerListController implements Initializable, DataChangeListener {
 	private TableColumn<Seller, String> tableColumnName;
 
 	@FXML
+	private TableColumn<Seller, String> tableColumnEmail;
+	
+	@FXML
+	private TableColumn<Seller, Date> tableColumnBirthDate;
+	
+	@FXML
+	private TableColumn<Seller, Double> tableColumnBaseSalary;
+
+	@FXML
 	private TableColumn<Seller, Seller> tableColumnEDIT;
 
 	@FXML
@@ -61,7 +71,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 
 	private void createDialogForm(Seller obj, String string, Stage parentStage) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setSellerService(SellerService service) {
@@ -76,6 +86,11 @@ public class SellerListController implements Initializable, DataChangeListener {
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+		tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+		Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+		tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+		Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
@@ -92,31 +107,24 @@ public class SellerListController implements Initializable, DataChangeListener {
 		initRemoveButtons();
 	}
 
-	/*private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			Pane pane = loader.load();
-			SellerFormController controller = loader.getController();
-			controller.setSeller(obj);
-			controller.setSellerService(new SellerService());
-
-			controller.subscribeDataChangeListener(this);
-			controller.updateFormData();
-
-			controller.updateFormData();
-
-			Stage dialogStage = new Stage();
-			dialogStage.setTitle("Enter Seller data");
-			dialogStage.setScene(new Scene(pane));
-			dialogStage.setResizable(false);
-			dialogStage.initOwner(parentStage);
-			dialogStage.initModality(Modality.WINDOW_MODAL);
-			dialogStage.showAndWait();
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-*/
+	/*
+	 * private void createDialogForm(Seller obj, String absoluteName, Stage
+	 * parentStage) { try { FXMLLoader loader = new
+	 * FXMLLoader(getClass().getResource(absoluteName)); Pane pane = loader.load();
+	 * SellerFormController controller = loader.getController();
+	 * controller.setSeller(obj); controller.setSellerService(new SellerService());
+	 * 
+	 * controller.subscribeDataChangeListener(this); controller.updateFormData();
+	 * 
+	 * controller.updateFormData();
+	 * 
+	 * Stage dialogStage = new Stage(); dialogStage.setTitle("Enter Seller data");
+	 * dialogStage.setScene(new Scene(pane)); dialogStage.setResizable(false);
+	 * dialogStage.initOwner(parentStage);
+	 * dialogStage.initModality(Modality.WINDOW_MODAL); dialogStage.showAndWait(); }
+	 * catch (IOException e) { Alerts.showAlert("IO Exception",
+	 * "Error loading view", e.getMessage(), AlertType.ERROR); } }
+	 */
 	@Override
 	public void onDataChanged() {
 		updateTableView();
@@ -136,8 +144,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 					return;
 				}
 				setGraphic(button);
-				button.setOnAction(
-						event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+				button.setOnAction(event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
 			}
 		});
 	}

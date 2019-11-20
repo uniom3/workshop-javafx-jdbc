87a1adf2f4e3acf82333fec0cd050330b69e900c
uniom3 <SDeivid.Mendonca@intranet.local>
2019-11-20 14:37:26 -0300

    Department ComboBox

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -31,6 +31,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 public class SellerListController implements Initializable, DataChangeListener {
@@ -114,13 +115,11 @@ public class SellerListController implements Initializable, DataChangeListener {
 			Pane pane = loader.load();
 			SellerFormController controller = loader.getController();
 			controller.setSeller(obj);
-			controller.setSellerService(new SellerService());
-
+			controller.setService(new SellerService(), new DepartmentService());
+			controller.loadAssociatedObjects();
 			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
-
 			controller.updateFormData();
-
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter Seller data");
 			dialogStage.setScene(new Scene(pane));
